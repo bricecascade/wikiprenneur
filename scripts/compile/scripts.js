cafb235385663b6c459f8d1c02e29a89a86274f1
CtrlX <Julien Coupez>
2014-06-19 21:16:29 -0400

    treeview test

--- scripts/compile/scripts.js
+++ scripts/compile/scripts.js
@@ -2,6 +2,5 @@
 //@prepros-append wiki/category-tabs.js
 //@prepros-append wiki/item-treeview.js
 //@prepros-append wiki/item-detail.js
-//@prepros-append min/jquery-2.1.1.min.js
+//@prepros-append vendor/jquery.js
 //@prepros-append foundation.js
-
