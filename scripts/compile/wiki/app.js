72e48ea6387b66b2371abc0ecc1cef1ef22fa20c
CtrlX <Julien Coupez>
2014-06-27 18:02:14 -0400

    added wikistore skeleton

--- scripts/compile/wiki/app.js
+++ scripts/compile/wiki/app.js
@@ -7,3 +7,39 @@
 	
 		
 })();
+
+
+
+var wikistore = {
+	name : "Nexonia",
+	tabs :[
+	{
+		name:"Montreal",
+		icon:"ico1",
+		children:[
+		{
+			name:"bloc 1",
+			icon:"ico2",
+			details:{
+			
+			},
+			children:[]
+		},
+		{
+			name:"bloc 2",
+			icon:"ico3",
+			details:{
+			},
+			children:[]
+		},
+		{
+			name:"bloc 3",
+			icon:"ico3",
+			details:{
+			},
+			children:[]
+		}
+		]
+	}
+	]
+};
